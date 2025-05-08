05e24fdc111d8738a968d5c2585fc6096b587306
ardrass <st117289@student.spbu.ru>
2025-05-08 23:01:20 +0300

    Add MoveGTEST

--- main.cc
+++ main.cc
@@ -1,6 +1,9 @@
 #include "Application.h"
+#include <Eigen/Core>
 
 int main(int argc, char *argv[]) {
+    Eigen::setNbThreads(std::thread::hardware_concurrency());
+    Eigen::initParallel();
     Application app(argc, argv);
     return app.exec();
 }
---
+++ tests/MoveGTEST.cc
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include "Scene.h"
+#include "FileOurP.h"
+
+TEST(MoveTest, LoadAndMove) {
+    Scene scene(nullptr);
+    
+    scene.loadFromFile("../testStructures/homeFINAL2.ourp");
+    
+    try {
+        scene.moveObject(ID(1), 10.0, 10.0);
+        
+        scene.moveObject(ID(3), 5.0, 5.0);
+        scene.moveObject(ID(92), -5.0, -5.0);
+        
+        SUCCEED();
+    } catch (const std::exception& e) {
+        FAIL() << "Exception occurred: " << e.what();
+    }
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+} 
