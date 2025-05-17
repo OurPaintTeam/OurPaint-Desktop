3ff8d46d75792d2739359ca2d73f103b8015b47a
Tim <timon.skvor@mail.ru>
2025-05-17 20:32:30 +0300

    Improved readability,Changed the signature, switched to the qt style

--- GUI/GUI_cc/Drawing/ClosesPoint.cc
+++ GUI/GUI_cc/Drawing/ClosesPoint.cc
@@ -1,52 +1,253 @@
 #include "ClosesPoint.h"
 
-QPointF ClosesPoint::findClosestPoint(std::unordered_map<ID, Point *> &points,double yourX,double yourY) {
-    if (points.size() > 0) {
-        QPointF closest;
-        double minDistance = std::numeric_limits<float>::max();
-
-        double cursorX = yourX;
-        double cursorY = yourY;
-
-        for (const auto &pt: points) {
-            const Point *point = pt.second;
-            double distance = std::sqrt(std::pow(point->x - cursorX, 2) + std::pow(point->y - cursorY, 2));
-            if (distance < minDistance) {
-                minDistance = distance;
-                closest = QPointF(point->x, point->y);
-            }
-        }
-
-        return closest;
+QPointF ClosesPoint::findClosestPoint(std::unordered_map<ID, Point *> &points,const QPointF cursor) {
+    if (points.size() == 0) {
+        return QPointF{};
     }
 
-    return QPointF{};
+    QPointF closest;
+    qreal minDistance = std::numeric_limits<qreal>::max();
+
+    for (const auto &pt: points) {
+        const Point *point = pt.second;
+        const qreal distance = qSqrt(qPow(point->x - cursor.x(), 2) +
+                                     qPow(point->y - cursor.y(), 2));
+        if (distance < minDistance) {
+            minDistance = distance;
+            closest = QPointF(point->x, point->y);
+        }
+    }
+
+    return closest;
 }
 
 
-bool ClosesPoint::checkFigure(double pointX, double pointY,double yourX,double yourY,double zoom) {
-    double RANGE = 3.0 / zoom;
+[[maybe_unused]] QVector<ID>
+ClosesPoint::enteringInRect(std::unordered_map<ID, Point *> &points, QRectF &rect, QVector<ID> &vec_id) {
 
-    double logicalX = yourX;
-    double logicalY = yourY;
+    vec_id.clear();
 
-    double dx = logicalX - pointX;
-    double dy = logicalY - pointY;
-    return std::sqrt(dx * dx + dy * dy) <= RANGE;
+    for (const auto &pt: points) {
+        const Point *point = pt.second;
+        const QPointF p(point->x, point->y);
+        if (rect.contains(p)) {
+            vec_id.push_back(pt.first);
+        }
+    }
+    return vec_id;
 }
 
 
-bool ClosesPoint::checkFigure(double x0, double y0, double r, double yourX,double yourY,double zoom) {
-    double cursorX = yourX;
-    double cursorY = yourY;
+[[maybe_unused]] QVector<ID>
+ClosesPoint::enteringInRect(std::unordered_map<ID, Circle *> &circles, QRectF &rect, QVector<ID> &vec_id) {
+    vec_id.clear();
+    for (const auto &pair: circles) {
+        const Circle *circle = pair.second;
+        const QPointF center(circle->center->x, circle->center->y);
+        const qreal radius = circle->r;
+        const qreal diam=2*radius;
 
-    double dx = (cursorX - x0);
-    double dy = (cursorY - y0);
-    double distance = std::sqrt(dx * dx + dy * dy);
+        QRectF circleBounds(center.x() - radius, center.y() - radius,
+                            diam, diam);
 
-    double RANGE = 3.0 / zoom;
+        if (rect.contains(circleBounds)) {
+            vec_id.push_back(pair.first);
+        }
+    }
+    return vec_id;
+}
 
-    if (std::abs(distance - r) <= RANGE) {
+
+[[maybe_unused]] QVector<ID>
+ClosesPoint::enteringInRect(std::unordered_map<ID, Section *> &sections, QRectF &rect, QVector<ID> &vec_id) {
+    vec_id.clear();
+    for (const auto &sec: sections) {
+        const Section *section = sec.second;
+        const QPointF p1(section->beg->x, section->beg->y);
+        const QPointF p2(section->end->x, section->end->y);
+
+        if (rect.contains(p1) || rect.contains(p2)) {
+            vec_id.push_back(sec.first);
+        } else {
+            const QLineF line(p1, p2);
+            QPolygonF polygon;
+            polygon << rect.topLeft() << rect.topRight()
+                    << rect.bottomRight() << rect.bottomLeft();
+
+            for (int i = 0; i < polygon.size(); ++i) {
+                const QLineF edge(polygon[i], polygon[(i + 1) % polygon.size()]);
+                QPointF intersection;
+                if (line.intersects(edge, &intersection) == QLineF::BoundedIntersection) {
+                    vec_id.push_back(sec.first);
+                    break;
+                }
+            }
+        }
+    }
+    return vec_id;
+}
+
+
+[[maybe_unused]] QVector<ID>
+ClosesPoint::enteringInRect(std::unordered_map<ID, Arc *> &arcs, QRectF &rect, QVector<ID> &vec_id) {
+    vec_id.clear();
+
+    for (const auto &elem : arcs) {
+        const ID &id = elem.first;
+        const Arc *arc = elem.second;
+
+        const QPointF beg(arc->beg->x, arc->beg->y);
+        const QPointF end(arc->end->x, arc->end->y);
+        const QPointF center(arc->center->x, arc->center->y);
+
+        const qreal radius = QLineF(center, beg).length();
+        qreal angleStart = QLineF(center, end).angle();
+        qreal angleEnd = QLineF(center, beg).angle();
+
+        angleStart = fmod(angleStart + 360.0, 360.0);
+        angleEnd = fmod(angleEnd + 360.0, 360.0);
+
+        qreal span = angleEnd - angleStart;
+        if (span <= 0.1) {
+            span += 360.0;
+        }
+        const qreal midAngleDeg = fmod(angleStart + span / 2.0, 360.0);
+        const qreal midAngleRad = qDegreesToRadians(midAngleDeg);
+
+        const QPointF mid(
+                center.x() + radius * qCos(midAngleRad),
+                center.y() - radius * qSin(midAngleRad)
+        );
+
+        if (rect.contains(beg) || rect.contains(end) || rect.contains(center) || rect.contains(mid)) {
+            vec_id.push_back(id);
+        }
+    }
+
+    return vec_id;
+}
+
+
+QVector<ID>
+ClosesPoint::enteringInRect(std::unordered_map<ID, Point *> &points, QRectF &rect) {
+    QVector<ID> vec_id;
+
+    for (const auto &pt: points) {
+        const Point *point = pt.second;
+        const QPointF p(point->x, point->y);
+        if (rect.contains(p)) {
+            vec_id.push_back(pt.first);
+        }
+    }
+    return vec_id;
+}
+
+
+QVector<ID>
+ClosesPoint::enteringInRect(std::unordered_map<ID, Circle *> &circles, QRectF &rect) {
+    QVector<ID> vec_id;
+    for (const auto &pair: circles) {
+        const Circle *circle = pair.second;
+        const QPointF center(circle->center->x, circle->center->y);
+        const qreal radius = circle->r;
+        const qreal diam = 2 * radius;
+
+        const QRectF circleBounds(center.x() - radius, center.y() - radius,diam, diam);
+
+        if (rect.contains(circleBounds)) {
+            vec_id.push_back(pair.first);
+        }
+    }
+    return vec_id;
+}
+
+
+QVector<ID>
+ClosesPoint::enteringInRect(std::unordered_map<ID, Section *> &sections, QRectF &rect) {
+    QVector<ID> vec_id;
+
+    for (const auto &sec: sections) {
+        const Section *section = sec.second;
+        const QPointF p1(section->beg->x, section->beg->y);
+        const QPointF p2(section->end->x, section->end->y);
+
+        if (rect.contains(p1) || rect.contains(p2)) {
+            vec_id.push_back(sec.first);
+        } else {
+            const QLineF line(p1, p2);
+            QPolygonF polygon;
+            polygon << rect.topLeft() << rect.topRight()
+                    << rect.bottomRight() << rect.bottomLeft();
+
+            for (int i = 0; i < polygon.size(); ++i) {
+                const QLineF edge(polygon[i], polygon[(i + 1) % polygon.size()]);
+                QPointF intersection;
+                if (line.intersects(edge, &intersection) == QLineF::BoundedIntersection) {
+                    vec_id.push_back(sec.first);
+                    break;
+                }
+            }
+        }
+    }
+    return vec_id;
+}
+
+
+QVector<ID>
+ClosesPoint::enteringInRect(std::unordered_map<ID, Arc *> &arcs, QRectF &rect) {
+    QVector<ID> vec_id;
+
+    for (const auto &elem : arcs) {
+        const Arc *arc = elem.second;
+
+        const QPointF beg(arc->beg->x, arc->beg->y);
+        const QPointF end(arc->end->x, arc->end->y);
+        const QPointF center(arc->center->x, arc->center->y);
+
+        const qreal radius = QLineF(center, beg).length();
+        qreal angleStart = QLineF(center, end).angle();
+        qreal angleEnd = QLineF(center, beg).angle();
+
+        angleStart = fmod(angleStart + 360.0, 360.0);
+        angleEnd = fmod(angleEnd + 360.0, 360.0);
+
+        qreal span = angleEnd - angleStart;
+        if (span <= 0.1) {
+            span += 360.0;
+        }
+
+        const qreal midAngleDeg = fmod(angleStart + span / 2.0, 360.0);
+        const qreal midAngleRad = qDegreesToRadians(midAngleDeg);
+
+        const QPointF mid(
+                center.x() + radius * std::cos(midAngleRad),
+                center.y() - radius * std::sin(midAngleRad)
+        );
+
+        if (rect.contains(beg) || rect.contains(end) || rect.contains(center) || rect.contains(mid)) {
+            vec_id.push_back(elem.first);
+        }
+    }
+
+    return vec_id;
+}
+
+
+bool ClosesPoint::checkFigure(const QPointF point,const QPointF cursor ,const qreal zoom) {
+    const qreal RANGE = 3.0 / zoom;
+    const QPointF delta(cursor - point);
+
+    return qSqrt(delta.x()*delta.x() + delta.y() * delta.y()) <= RANGE;
+}
+
+
+bool ClosesPoint::checkFigure(const QPointF center, qreal radius, const QPointF cursor,const qreal zoom) {
+
+    const QPointF delta(cursor-center);
+    const qreal distance = qSqrt(delta.x()*delta.x() + delta.y() * delta.y());
+    const qreal RANGE = 3.0 / zoom;
+
+    if (qAbs(distance - radius) <= RANGE) {
         return true;
     }
 
@@ -54,21 +255,31 @@ bool ClosesPoint::checkFigure(double x0, double y0, double r, double yourX,doubl
 }
 
 
-[[maybe_unused]] bool ClosesPoint::checkFigure(double x0, double y0, double x1, double y1, double xc, double yc,double yourX,double yourY,double zoom) {
-    double cursorX = yourX;
-    double cursorY = yourY;
+bool ClosesPoint::checkFigure(const QPointF startPoint, const QPointF endPoint, const QPointF cursor,const qreal zoom) {
+    const qreal RANGE = 5.0 / zoom;
 
-    double r = std::hypot(x0 - xc, y0 - yc);
-    double distToCenter = std::hypot(cursorX - xc, cursorY - yc);
+    qreal distance = distancePointToSection(cursor, startPoint, endPoint);
 
-    double RANGE = 3.0 / zoom;
-    if (std::abs(distToCenter - r) > RANGE) {
+    if (distance <= RANGE) {
+        return true;
+    }
+
+    return false;
+}
+
+
+[[maybe_unused]] bool ClosesPoint::checkFigure(const QPointF startPoint, const QPointF endPoint, const QPointF centerPoint,const QPointF cursor,const qreal zoom) {
+    const qreal radius = std::hypot(startPoint.x() - centerPoint.x(), startPoint.y() - centerPoint.y());
+    const qreal distToCenter = std::hypot(cursor.x() - centerPoint.x(), cursor.y() - centerPoint.y());
+
+    const qreal RANGE = 3.0 / zoom;
+    if (std::abs(distToCenter - radius) > RANGE) {
         return false;
     }
 
-    double angleEnd = QLineF(QPointF(xc, yc), QPointF(x0, y0)).angle();
-    double angleStart = QLineF(QPointF(xc, yc), QPointF(x1, y1)).angle();
-    double angleCursor = QLineF(QPointF(xc, yc), QPointF(cursorX, cursorY)).angle();
+    qreal angleEnd = QLineF(centerPoint, startPoint).angle();
+    qreal angleStart = QLineF(centerPoint, endPoint).angle();
+    qreal angleCursor = QLineF(centerPoint, cursor).angle();
 
     angleStart = fmod(angleStart + 360.0, 360.0);
     angleEnd = fmod(angleEnd + 360.0, 360.0);
@@ -82,246 +293,30 @@ bool ClosesPoint::checkFigure(double x0, double y0, double r, double yourX,doubl
 }
 
 
-bool ClosesPoint::checkFigure(double x0, double y0, double x1, double y1,double yourX,double yourY,double zoom) {
-
-    double RANGE = 5.0 / zoom;
-
-    double mouseX =yourX;
-    double mouseY = yourY;
-
-    double distance = distancePointToSection(mouseX, mouseY, x0, y0, x1, y1);
-
-    if (distance <= RANGE) {
-        return true;
+qreal ClosesPoint::distancePointToSection(const QPointF cursor, const QPointF startPoint, const QPointF endPoint) {
+    const QPointF segment = endPoint - startPoint;
+    const qreal dx = segment.x();
+    const qreal dy = segment.y();
+    
+    if (qFuzzyIsNull(dx) && qFuzzyIsNull(dy)) {
+        return QLineF(cursor, startPoint).length();
     }
 
-    return false;
+    const QPointF toCursor = cursor - startPoint;
+    
+    const qreal dot = QPointF::dotProduct(toCursor, segment);
+    const qreal lenSq = QPointF::dotProduct(segment, segment);
+    
+    qreal t = dot / lenSq;
+    t = qBound(0.0, t, 1.0);
+
+    const QPointF projection = startPoint + segment * t;
+
+    return QLineF(cursor, projection).length();
 }
 
 
-double ClosesPoint::distancePointToSection(double px, double py, double x0, double y0, double x1, double y1) {
-    double dx = x1 - x0;
-    double dy = y1 - y0;
-
-    if (dx * dx + dy * dy == 0.0) {
-        dx = px - x0;
-        dy = py - y0;
-        return std::sqrt(dx * dx + dy * dy);
-    }
-
-    // The t parameter for the projection
-    double t = ((px - x0) * dx + (py - y0) * dy) / (dx * dx + dy * dy);
-
-    t = std::max(0.0, std::min(1.0, t));
-
-    // The nearest point on the segment
-    double nearestX = x0 + t * dx;
-    double nearestY = y0 + t * dy;
-
-    // Distance to the nearest point
-    dx = px - nearestX;
-    dy = py - nearestY;
-
-    return std::sqrt(dx * dx + dy * dy);
-}
-
-[[maybe_unused]] std::vector<ID>
-ClosesPoint::enteringInRect(std::unordered_map<ID, Point *> &points, QRectF &rect, std::vector<ID> &vec_id) {
-
-    vec_id.clear();
-
-    for (const auto &pt: points) {
-        const Point *point = pt.second;
-        QPointF p(point->x, point->y);
-        if (rect.contains(p)) {
-            vec_id.push_back(pt.first);
-        }
-    }
-    return vec_id;
-}
-
-[[maybe_unused]] std::vector<ID>
-ClosesPoint::enteringInRect(std::unordered_map<ID, Circle *> &circles, QRectF &rect, std::vector<ID> &vec_id) {
-    vec_id.clear();
-    for (const auto &pair: circles) {
-        const Circle *circle = pair.second;
-        QPointF center(circle->center->x, circle->center->y);
-        double radius = circle->r;
-
-        QRectF circleBounds(center.x() - radius, center.y() - radius,
-                            2 * radius, 2 * radius);
-
-        if (rect.contains(circleBounds)) {
-            vec_id.push_back(pair.first);
-        }
-    }
-    return vec_id;
-}
-
-[[maybe_unused]] std::vector<ID>
-ClosesPoint::enteringInRect(std::unordered_map<ID, Section *> &sections, QRectF &rect, std::vector<ID> &vec_id) {
-    vec_id.clear();
-    for (const auto &sec: sections) {
-        const Section *section = sec.second;
-        QPointF p1(section->beg->x, section->beg->y);
-        QPointF p2(section->end->x, section->end->y);
-
-        if (rect.contains(p1) || rect.contains(p2)) {
-            vec_id.push_back(sec.first);
-        } else {
-            QLineF line(p1, p2);
-            QPolygonF polygon;
-            polygon << rect.topLeft() << rect.topRight()
-                    << rect.bottomRight() << rect.bottomLeft();
-
-            for (int i = 0; i < polygon.size(); ++i) {
-                QLineF edge(polygon[i], polygon[(i + 1) % polygon.size()]);
-                QPointF intersection;
-                if (line.intersects(edge, &intersection) == QLineF::BoundedIntersection) {
-                    vec_id.push_back(sec.first);
-                    break;
-                }
-            }
-        }
-    }
-    return vec_id;
-}
 
 
-[[maybe_unused]] std::vector<ID>
-ClosesPoint::enteringInRect(std::unordered_map<ID, Arc *> &arcs, QRectF &rect, std::vector<ID> &vec_id) {
-    vec_id.clear();
 
-    for (const auto &elem : arcs) {
-        const ID &id = elem.first;
-        const Arc *arc = elem.second;
-
-        QPointF beg(arc->beg->x, arc->beg->y);
-        QPointF end(arc->end->x, arc->end->y);
-        QPointF center(arc->center->x, arc->center->y);
-
-        double r = QLineF(center, beg).length();
-        double angleStart = QLineF(center, end).angle();
-        double angleEnd = QLineF(center, beg).angle();
-
-        angleStart = fmod(angleStart + 360.0, 360.0);
-        angleEnd = fmod(angleEnd + 360.0, 360.0);
-
-        double span = angleEnd - angleStart;
-        if (span <= 0.1) { span += 360.0; }
-        double midAngleDeg = fmod(angleStart + span / 2.0, 360.0);
-        double midAngleRad = qDegreesToRadians(midAngleDeg);
-
-        QPointF mid(
-                center.x() + r * std::cos(midAngleRad),
-                center.y() - r * std::sin(midAngleRad)
-        );
-
-        if (rect.contains(beg) || rect.contains(end) || rect.contains(center) || rect.contains(mid)) {
-            vec_id.push_back(id);
-        }
-    }
-
-    return vec_id;
-}
-
-
-std::vector<ID>
-ClosesPoint::enteringInRect(std::unordered_map<ID, Point *> &points, QRectF &rect) {
-    std::vector<ID> vec_id;
-
-    for (const auto &pt: points) {
-        const Point *point = pt.second;
-        QPointF p(point->x, point->y);
-        if (rect.contains(p)) {
-            vec_id.push_back(pt.first);
-        }
-    }
-    return vec_id;
-}
-
-std::vector<ID>
-ClosesPoint::enteringInRect(std::unordered_map<ID, Circle *> &circles, QRectF &rect) {
-    std::vector<ID> vec_id;
-    for (const auto &pair: circles) {
-        const Circle *circle = pair.second;
-        QPointF center(circle->center->x, circle->center->y);
-        double radius = circle->r;
-
-        QRectF circleBounds(center.x() - radius, center.y() - radius,
-                            2 * radius, 2 * radius);
-
-        if (rect.contains(circleBounds)) {
-            vec_id.push_back(pair.first);
-        }
-    }
-    return vec_id;
-}
-
-std::vector<ID>
-ClosesPoint::enteringInRect(std::unordered_map<ID, Section *> &sections, QRectF &rect) {
-    std::vector<ID> vec_id;
-    for (const auto &sec: sections) {
-        const Section *section = sec.second;
-        QPointF p1(section->beg->x, section->beg->y);
-        QPointF p2(section->end->x, section->end->y);
-
-        if (rect.contains(p1) || rect.contains(p2)) {
-            vec_id.push_back(sec.first);
-        } else {
-            QLineF line(p1, p2);
-            QPolygonF polygon;
-            polygon << rect.topLeft() << rect.topRight()
-                    << rect.bottomRight() << rect.bottomLeft();
-
-            for (int i = 0; i < polygon.size(); ++i) {
-                QLineF edge(polygon[i], polygon[(i + 1) % polygon.size()]);
-                QPointF intersection;
-                if (line.intersects(edge, &intersection) == QLineF::BoundedIntersection) {
-                    vec_id.push_back(sec.first);
-                    break;
-                }
-            }
-        }
-    }
-    return vec_id;
-}
-
-
-std::vector<ID>
-ClosesPoint::enteringInRect(std::unordered_map<ID, Arc *> &arcs, QRectF &rect) {
-    std::vector<ID> vec_id;
-    for (const auto &elem : arcs) {
-        const Arc *arc = elem.second;
-
-        QPointF beg(arc->beg->x, arc->beg->y);
-        QPointF end(arc->end->x, arc->end->y);
-        QPointF center(arc->center->x, arc->center->y);
-
-        double r = QLineF(center, beg).length();
-        double angleStart = QLineF(center, end).angle();
-        double angleEnd = QLineF(center, beg).angle();
-
-        angleStart = fmod(angleStart + 360.0, 360.0);
-        angleEnd = fmod(angleEnd + 360.0, 360.0);
-
-        double span = angleEnd - angleStart;
-        if (span <= 0.1) {
-            span += 360.0;
-        }
-        double midAngleDeg = fmod(angleStart + span / 2.0, 360.0);
-        double midAngleRad = qDegreesToRadians(midAngleDeg);
-
-        QPointF mid(
-                center.x() + r * std::cos(midAngleRad),
-                center.y() - r * std::sin(midAngleRad)
-        );
-
-        if (rect.contains(beg) || rect.contains(end) || rect.contains(center) || rect.contains(mid)) {
-            vec_id.push_back(elem.first);
-        }
-    }
-
-    return vec_id;
-}
 
--- GUI/GUI_h/Drawing/ClosesPoint.h
+++ GUI/GUI_h/Drawing/ClosesPoint.h
@@ -20,29 +20,28 @@ class ClosesPoint {
 
 public:
     // Nearest point search function
-    static QPointF findClosestPoint(std::unordered_map<ID,  Point*>& points,double yourX,double yourY);
+    static QPointF findClosestPoint(std::unordered_map<ID, Point *> &points,const QPointF cursor);
 
     // Checking for entry into a rectangle
+    [[maybe_unused]] static QVector<ID>  enteringInRect(std::unordered_map<ID,  Point*>& points,QRectF &rect,QVector<ID>&);
+    [[maybe_unused]] static QVector<ID>  enteringInRect(std::unordered_map<ID, Section*>& sections,QRectF &rect,QVector<ID>&);
+    [[maybe_unused]] static QVector<ID>  enteringInRect(std::unordered_map<ID,  Circle*>& circles,QRectF &rect,QVector<ID>&);
+    [[maybe_unused]] static QVector<ID>  enteringInRect(std::unordered_map<ID,  Arc*>& arcs,QRectF &rect,QVector<ID>&);
 
-    [[maybe_unused]] static std::vector<ID>  enteringInRect(std::unordered_map<ID,  Point*>& points,QRectF &rect,std::vector<ID>&);
-    [[maybe_unused]] static std::vector<ID>  enteringInRect(std::unordered_map<ID, Section*>& sections,QRectF &rect,std::vector<ID>&);
-    [[maybe_unused]] static std::vector<ID>  enteringInRect(std::unordered_map<ID,  Circle*>& circles,QRectF &rect,std::vector<ID>&);
-    [[maybe_unused]] static std::vector<ID>  enteringInRect(std::unordered_map<ID,  Arc*>& arcs,QRectF &rect,std::vector<ID>&);
-
-    static std::vector<ID> enteringInRect(std::unordered_map<ID, Arc *> &arcs, QRectF &rect);
-    static std::vector<ID> enteringInRect(std::unordered_map<ID, Section *> &sections, QRectF &rect);
-    static std::vector<ID> enteringInRect(std::unordered_map<ID, Circle *> &circles, QRectF &rect);
-    static  std::vector<ID> enteringInRect(std::unordered_map<ID, Point *> &points, QRectF &rect);
+    static QVector<ID> enteringInRect(std::unordered_map<ID, Arc *> &arcs, QRectF &rect);
+    static QVector<ID> enteringInRect(std::unordered_map<ID, Section *> &sections, QRectF &rect);
+    static QVector<ID> enteringInRect(std::unordered_map<ID, Circle *> &circles, QRectF &rect);
+    static QVector<ID> enteringInRect(std::unordered_map<ID, Point *> &points, QRectF &rect);
 
     // The function of checking the coordinates of the cursor and the figure to move
-    static bool checkFigure(double pointX, double pointY, double yourX,double yourY,double zoom);
-    static bool checkFigure(double x0, double y0, double r, double yourX,double yourY,double zoom);
-    static bool checkFigure(double x0, double y0, double x1, double y1,double yourX,double yourY,double zoom);
+    static bool checkFigure(const QPointF point,const QPointF cursor ,const qreal zoom);
+    static bool checkFigure(const QPointF center, qreal radius, const QPointF cursor,const qreal zoom);
+    static bool checkFigure(const QPointF startPoint, const QPointF endPoint, const QPointF cursor,const qreal zoom);
 
-    [[maybe_unused]] static bool checkFigure(double x0, double y0, double x1, double y1,double xc, double yc,double yourX,double yourY,double zoom);
+    [[maybe_unused]] static bool checkFigure(const QPointF startPoint, const QPointF endPoint, const QPointF centerPoint,const QPointF cursor,const qreal zoom);
 
     // Auxiliary function for checking cursor and line
-    static double distancePointToSection(double px, double py, double x0, double y0, double x1, double y1);
+    static double distancePointToSection(const QPointF cursor, const QPointF startPoint, const QPointF endPoint);
 };
 
 #endif //OURPAINT_CLOSESPOINT_H
