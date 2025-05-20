a5f1e7e2bc945cc5f5710bfd3cf7bbdb0852ce58
Tim <timon.skvor@mail.ru>
2025-05-20 15:07:16 +0300

    Delete unnecessary code

--- GUI/GUI_h/Drawing/ClosestPoint.h
+++ GUI/GUI_h/Drawing/ClosestPoint.h
@@ -23,18 +23,6 @@ public:
     static QPointF findClosestPoint(const std::unordered_map<ID, Point*>& points, const QPointF cursor);
 
     // Checking for entry into a rectangle
-    [[maybe_unused]]
-    static QVector<ID> enteringInRect(const std::unordered_map<ID, Point*>& points,const QRectF& rect,QVector<ID>&);
-
-    [[maybe_unused]]
-    static QVector<ID> enteringInRect(const std::unordered_map<ID, Section*>& sections,const QRectF& rect,QVector<ID>&);
-
-    [[maybe_unused]]
-    static QVector<ID> enteringInRect(const std::unordered_map<ID, Circle*>& circles,const QRectF& rect,QVector<ID>&);
-
-    [[maybe_unused]]
-    static QVector<ID> enteringInRect(const std::unordered_map<ID, Arc*>& arcs,const QRectF& rect, QVector<ID>&);
-
     static QPolygonF rectToPolygon(const QRectF& rect);
     static bool pointInRect(const Point* pt, const QRectF& rect);
     static bool circleInRect(const Circle* circle, const QRectF& rect);
