21f1e1880dc80ebc3bb6e833d8236e0265f09ddc
ardrass <st117289@student.spbu.ru>
2025-06-30 22:11:53 +0300

    added pair in obj in json

--- headers/saveload/objectInJson.h
+++ headers/saveload/objectInJson.h
@@ -5,19 +5,24 @@
 #ifndef OBJECTINJSON_H
 #define OBJECTINJSON_H
 #include <nlohmann/json.hpp>
+
 #include "Objects.h"
 class objectInJson {
   std::vector<std::string> _name;
   ObjectData _obj;
   std::vector<ObjectData> _subObjects;
-public:
-  objectInJson(const std::vector<std::string>& name, const ObjectData& obj);
+
+ public:
+  objectInJson(const std::vector<std::string> &name, const ObjectData &obj);
   objectInJson(const objectInJson &other);
   objectInJson(objectInJson &&other) noexcept;
   objectInJson &operator=(const objectInJson &other);
   objectInJson &operator=(objectInJson &&other) noexcept;
   nlohmann::json to_json() const;
-  void from_json(const nlohmann::json& obj);
+  void from_json(const nlohmann::json &obj);
+  std::pair<std::vector<std::string>, ObjectData> to_pair() const {
+    return std::make_pair(_name, _obj);
+  }
 };
 
-#endif //OBJECTINJSON_H
+#endif  // OBJECTINJSON_H
