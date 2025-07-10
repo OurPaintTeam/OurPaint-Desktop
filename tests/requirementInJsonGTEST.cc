1b9c82091eaf1b929b8071d2e254b6c2c902015d
Ibragim Oskanov <oskanov-ibragim.06@mail.ru>
2025-07-10 21:46:55 +0300

    Change test CMakeLists: Test for requirementInJson

--- tests/requirementInJsonGTEST.cc
+++ tests/requirementInJsonGTEST.cc
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include <nlohmann/json.hpp>
-#include "saveload/requirementInJson.h"
+#include "requirementInJson.h"
 
 
 TEST(RequirementInJsonTest, SerializeDeserialize) {
