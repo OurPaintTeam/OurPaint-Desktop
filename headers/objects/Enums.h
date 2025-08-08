9570edd3412281e7fef469be2c1d26df77403dd3
Ibragim Oskanov <oskanov-ibragim.06@mail.ru>
2025-08-08 21:19:08 +0300

    Review ReqRule array

--- headers/objects/Enums.h
+++ headers/objects/Enums.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <cstdint>
+#include <span>
 
 enum class ObjType : uint8_t {
     ET_POINT, ET_SECTION, ET_CIRCLE, ET_ARC
@@ -24,24 +25,29 @@ enum class ReqType : uint8_t {
 };
 
 struct ReqRule {
-    ObjType first;
-    ObjType second;
+    std::span<const ObjType> types;
     bool symmetric;
     bool needsParam;
 };
 
+constexpr ObjType P_S[] = {ObjType::ET_POINT, ObjType::ET_SECTION};
+constexpr ObjType P_P[] = {ObjType::ET_POINT, ObjType::ET_POINT};
+constexpr ObjType S_C[] = {ObjType::ET_SECTION, ObjType::ET_CIRCLE};
+constexpr ObjType S_S[] = {ObjType::ET_SECTION, ObjType::ET_SECTION};
+constexpr ObjType P_P_P[] = {ObjType::ET_POINT, ObjType::ET_POINT, ObjType::ET_POINT};
+
 static constexpr ReqRule ReqRules[static_cast<std::size_t>(ReqType::COUNT)] = {
-     /* ET_POINTSECTIONDIST            */ {ObjType::ET_POINT,   ObjType::ET_SECTION, true , true },
-     /* ET_POINTONSECTION              */ {ObjType::ET_POINT,   ObjType::ET_SECTION, false, false},
-     /* ET_POINTPOINTDIST              */ {ObjType::ET_POINT,   ObjType::ET_POINT,   true , true },
-     /* ET_POINTONPOINT                */ {ObjType::ET_POINT,   ObjType::ET_POINT,   false, false},
-     /* ET_SECTIONCIRCLEDIST           */ {ObjType::ET_SECTION, ObjType::ET_CIRCLE,  true , true },
-     /* ET_SECTIONONCIRCLE             */ {ObjType::ET_SECTION, ObjType::ET_CIRCLE,  false, false},
-     /* ET_SECTIONINCIRCLE             */ {ObjType::ET_SECTION, ObjType::ET_CIRCLE,  false, false},
-     /* ET_SECTIONSECTIONPARALLEL      */ {ObjType::ET_SECTION, ObjType::ET_SECTION, true , false},
-     /* ET_SECTIONSECTIONPERPENDICULAR */ {ObjType::ET_SECTION,ObjType::ET_SECTION, true , false},
-     /* ET_SECTIONSECTIONANGLE         */ {ObjType::ET_SECTION, ObjType::ET_SECTION, false, true },
-     /* ET_ARCCENTERONPERPENDICULAR    */ {ObjType::ET_POINT,     ObjType::ET_POINT, true, false}
+    { P_S , true , true  }, // ET_POINTSECTIONDIST
+    { P_S , false, false }, // ET_POINTONSECTION
+    { P_P , true , true  }, // ET_POINTPOINTDIST
+    { P_P , false, false }, // ET_POINTONPOINT
+    { S_C , true , true  }, // ET_SECTIONCIRCLEDIST
+    { S_C , false, false }, // ET_SECTIONONCIRCLE
+    { S_C , false, false }, // ET_SECTIONINCIRCLE
+    { S_S , true , false }, // ET_SECTIONSECTIONPARALLEL
+    { S_S , true , false }, // ET_SECTIONSECTIONPERPENDICULAR
+    { S_S , false, true  }, // ET_SECTIONSECTIONANGLE
+    { P_P_P, true , false} // ET_ARCCENTERONPERPENDICULAR
 };
 
 inline std::string to_string(ObjType el) {
