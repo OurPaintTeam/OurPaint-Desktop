e4c6bdbf6e8582fbae9ac67bd91eb73477ea4924
Ibragim Oskanov <oskanov-ibragim.06@mail.ru>
2025-08-11 20:07:46 +0300

    Fix Component test

--- tests/ComponentTest.cc
+++ tests/ComponentTest.cc
@@ -32,9 +32,16 @@ TEST(ComponentTest, MoveConstructor_and_MoveOperator) {
 
     std::unordered_set<ID> objectIDs = { ID(1), ID(2) };
 
+    std::vector<Variable*> varsCopy = {
+            new Variable(x1),
+            new Variable(y1),
+            new Variable(x2),
+            new Variable(y2)
+    };
+
     Component component;
     component._errorFunctions = errorFunctions;
-    component._vars = vars;
+    component._vars = varsCopy;
     component._task = task;
     component._objectIDs = objectIDs;
     Component c2(std::move(component));
@@ -45,7 +52,7 @@ TEST(ComponentTest, MoveConstructor_and_MoveOperator) {
     EXPECT_EQ(component._errorFunctions.size(), 0);
 
     EXPECT_EQ(c2._task, task);
-    EXPECT_EQ(c2._vars, vars);
+    EXPECT_EQ(c2._vars, varsCopy);
     EXPECT_EQ(c2._errorFunctions, errorFunctions);
     EXPECT_EQ(c2._objectIDs, objectIDs);
 
@@ -58,7 +65,7 @@ TEST(ComponentTest, MoveConstructor_and_MoveOperator) {
     EXPECT_EQ(c2._errorFunctions.size(), 0);
 
     EXPECT_EQ(c3._task, task);
-    EXPECT_EQ(c3._vars, vars);
+    EXPECT_EQ(c3._vars, varsCopy);
     EXPECT_EQ(c3._errorFunctions, errorFunctions);
     EXPECT_EQ(c3._objectIDs, objectIDs);
 }
