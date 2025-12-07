cd0717999363c1ce74373df0eb80eecee5dadba0
Ibragim Oskanov <oskanov-ibragim.06@mail.ru>
2025-12-07 23:30:33 +0300

    Review CMakeLists-s

---
+++ src/GUI/GUI_h/Widgets/ParameterDelegate.h
@@ -0,0 +1,35 @@
+#ifndef OURPAINT_PARAMETERDELEGATE_H
+#define OURPAINT_PARAMETERDELEGATE_H
+
+#include <QApplication>
+#include <QHBoxLayout>
+#include <QItemDelegate>
+#include <QLineEdit>
+#include <QPainter>
+#include <QString>
+#include <QStyledItemDelegate>
+#include <QTimer>
+
+#include "ProtectedPrefixLineEdit.h"
+
+// A class for changing data in the left menu
+
+class ParameterDelegate : public QStyledItemDelegate {
+Q_OBJECT
+public:
+    using QStyledItemDelegate::QStyledItemDelegate;
+
+    QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
+    void setEditorData(QWidget* editor, const QModelIndex& index) const override;
+    void setModelData(QWidget* editor, QAbstractItemModel* model, const QModelIndex& index) const override;
+
+    bool
+    editorEvent(QEvent* event, QAbstractItemModel* model, const QStyleOptionViewItem& option,
+                const QModelIndex& index) override;
+
+signals:
+
+    void doubleClickOnID(const QModelIndex& index);
+};
+
+#endif //OURPAINT_PARAMETERDELEGATE_H
