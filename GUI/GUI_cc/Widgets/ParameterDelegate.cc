e5a87d48fa0c6b21a6ead3eb896c65061d543ab8
Tim <timon.skvor@mail,ru>
2026-02-08 20:46:08 +0300

    Add qmenu in parameter delegate

--- GUI/GUI_cc/Widgets/ParameterDelegate.cc
+++ GUI/GUI_cc/Widgets/ParameterDelegate.cc
@@ -1,5 +1,7 @@
 #include "ParameterDelegate.h"
 
+#include <QMenu>
+
 QWidget* ParameterDelegate::createEditor(QWidget* parent, const QStyleOptionViewItem&, const QModelIndex& index) const {
     const QString text = index.data(Qt::DisplayRole).toString();
 
@@ -74,6 +76,23 @@ bool ParameterDelegate::editorEvent(QEvent* event,
                                     const QStyleOptionViewItem& option,
                                     const QModelIndex& index) {
 
+    if (event->type() == QEvent::MouseButtonPress ) {
+        QMouseEvent* mouseEvent = static_cast<QMouseEvent*>(event);
+        if ( mouseEvent->button() == Qt::RightButton) {
+            TreeNode* node = static_cast<TreeNode*>(index.internalPointer());
+            if (!node) {
+                return QStyledItemDelegate::editorEvent(event, model, option, index);
+            }
+
+            if (node->parent()) {
+                if ((node->parent()->getName() == "Projects")) {
+                    showContextMenu(mouseEvent->globalPosition().toPoint(), index,node);
+                }
+            }
+        }
+
+    }
+
     if (event->type() == QEvent::MouseButtonDblClick) {
 
         TreeNode* node = static_cast<TreeNode*>(index.internalPointer());
@@ -136,4 +155,64 @@ void ParameterDelegate::paint(QPainter* painter,
     painter->setPen(QColor(220, 220, 220));
     painter->drawText(crossRect, Qt::AlignCenter, "×");
     painter->restore();
+}
+
+
+void ParameterDelegate::showContextMenu(const QPoint globalPos, const QModelIndex& index,TreeNode* node) {
+    if (!node) {
+        return;
+    }
+
+    QString nodeName = node->getName();
+
+    QMenu contextMenu;
+    contextMenu.setStyleSheet(
+        "QMenu {"
+        "    background-color: #494850;"
+        "    border: 1px solid #333333;"
+        "    border-radius: 5px;"
+        "    padding: 5px;"
+        "}"
+        "QMenu::item {"
+        "    background-color: transparent;"
+        "    color: #D8D8F6;"
+        "    padding: 8px 20px;"
+        "    border-radius: 3px;"
+        "}"
+        "QMenu::item:selected {"
+        "    background-color: #2f4557;"
+        "}"
+        "QMenu::separator {"
+        "    height: 1px;"
+        "    background: #333333;"
+        "    margin: 5px 10px;"
+        "}"
+    );
+
+    QAction* openAction = contextMenu.addAction("Open");
+    QAction* renameAction = contextMenu.addAction( "Rename");
+    QAction* deleteAction = contextMenu.addAction("Delete");
+    contextMenu.addSeparator();
+
+    QString menuTitle = QString("Project: %1").arg(nodeName);
+    QAction* titleAction = new QAction(menuTitle, &contextMenu);
+    titleAction->setEnabled(false);
+    contextMenu.insertAction(contextMenu.actions().first(), titleAction);
+    contextMenu.insertSeparator(contextMenu.actions().at(1));
+
+    QAction* selectedAction = contextMenu.exec(globalPos);
+
+    if (!selectedAction) {
+        return;
+    }
+
+    if (selectedAction == openAction) {
+        emit doubleClickOnProject(node);
+    }
+    else if (selectedAction == renameAction) {
+        emit renameRequested(index);
+    }
+    else if (selectedAction == deleteAction) {
+        emit deleteClicked(node);
+    }
 }
--- GUI/GUI_h/Widgets/ParameterDelegate.h
+++ GUI/GUI_h/Widgets/ParameterDelegate.h
@@ -16,26 +16,32 @@
 // A class for changing data in the left menu
 
 class ParameterDelegate : public QStyledItemDelegate {
-Q_OBJECT
+    Q_OBJECT
+
 public:
     using QStyledItemDelegate::QStyledItemDelegate;
 
-    QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
-    void setEditorData(QWidget* editor, const QModelIndex& index) const override;
-    void setModelData(QWidget* editor, QAbstractItemModel* model, const QModelIndex& index) const override;
+    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+
+    void setEditorData(QWidget *editor, const QModelIndex &index) const override;
+
+    void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const override;
 
     bool
-    editorEvent(QEvent* event, QAbstractItemModel* model, const QStyleOptionViewItem& option,
-                const QModelIndex& index) override;
+    editorEvent(QEvent *event, QAbstractItemModel *model, const QStyleOptionViewItem &option,
+                const QModelIndex &index) override;
 
-    void paint(QPainter* painter,
-                              const QStyleOptionViewItem& option,
-                              const QModelIndex& index) const override;
+    void paint(QPainter *painter,
+               const QStyleOptionViewItem &option,
+               const QModelIndex &index) const override;
+
+    void showContextMenu(QPoint globalPos, const QModelIndex& index,TreeNode* node);
 
 signals:
-    void deleteClicked(TreeNode* node);
-    void doubleClickOnProject(TreeNode* node);
-    void doubleClickOnID(const QModelIndex& index);
+    void deleteClicked(TreeNode *node);
+    void doubleClickOnProject(TreeNode *node);
+    void doubleClickOnID(const QModelIndex &index);
+    void renameRequested(const QModelIndex& index);
 };
 
 #endif //OURPAINT_PARAMETERDELEGATE_H
