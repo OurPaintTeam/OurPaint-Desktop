f0e8d9f5fa17c6023970c2cf5ab849de72d3d71c
ardrass2 <ardrass@mail.ru>
2026-04-30 02:19:33 +0300

    Add document save/load actions

--- src/app/UIController.cc
+++ src/app/UIController.cc
@@ -1,16 +1,28 @@
 #include "UIController.h"
 #include "DocumentManager.h"
+#include "saveload/DocumentSaveLoad.h"
 #include "Transaction.h"
 #include "ConsoleManager.h"
 #include "UndoRedo.h"
 #include "Document.h"
 #include "IViewportHost.h"
 
+#include <filesystem>
 #include <iostream>
 
 #include "QDebug"
-UIController::UIController(EditorSession& editorSession, DocumentManager& manager, IViewportHost& host)
-    : editorSession_(editorSession), docManager_(manager), host_(host) {}
+UIController::UIController(EditorSession& editorSession,
+                           DocumentManager& manager,
+                           IViewportHost& host,
+                           RenderDataBuilder& builder,
+                           Cpu2dPicker& picker,
+                           OverlayModel& overlay)
+    : editorSession_(editorSession),
+      docManager_(manager),
+      host_(host),
+      builder_(builder),
+      picker_(picker),
+      overlay_(overlay) {}
 
 void UIController::selectTool(ToolId tool) {
     editorSession_.select(tool);
@@ -38,19 +50,100 @@ void UIController::openProjectInCurrentWindow() {}
 
 void UIController::createProjectInCurrentWindow() {}
 
-void UIController::openFile() {}
+std::string UIController::openFile(const std::string& filePath) {
+    const int index = docManager_.openDocument(filePath);
+    if (index < 0) {
+        return {};
+    }
+
+    syncActiveDocument();
+    const Document* document = docManager_.getActiveDocument();
+    return document ? document->name() : std::string{};
+}
+
+bool UIController::saveActiveDocument() {
+    Document* document = docManager_.getActiveDocument();
+    if (document == nullptr) {
+        return false;
+    }
+    if (!saveDocumentToFile(document->path(), *document)) {
+        return false;
+    }
+    document->markClean();
+    return true;
+}
+
+std::string UIController::saveActiveDocumentAs(const std::string& filePath) {
+    Document* document = docManager_.getActiveDocument();
+    if (document == nullptr || filePath.empty()) {
+        return {};
+    }
+
+    const std::string oldName = document->name();
+    const std::string oldPath = document->path();
+    std::string newName = std::filesystem::path(filePath).stem().string();
+    if (newName.empty()) {
+        newName = oldName;
+    }
+
+    document->setName(newName);
+    document->setPath(filePath);
+    if (!saveDocumentToFile(filePath, *document)) {
+        document->setName(oldName);
+        document->setPath(oldPath);
+        return {};
+    }
+
+    document->markClean();
+    return document->name();
+}
+
+bool UIController::setActiveDocument(const std::string& documentName) {
+    if (!docManager_.setActiveDocument(documentName)) {
+        return false;
+    }
+    syncActiveDocument();
+    return true;
+}
+
+std::string UIController::activeDocumentName() {
+    const Document* document = docManager_.getActiveDocument();
+    return document ? document->name() : std::string{};
+}
 
 void UIController::renameTab() {}
 
 void UIController::removeTab() {}
 
-void UIController::createFile() {}
+std::string UIController::createFile(const std::string& documentName) {
+    const int index = docManager_.createNewDocument(documentName);
+    if (index < 0) {
+        return {};
+    }
+
+    syncActiveDocument();
+    const Document* document = docManager_.getActiveDocument();
+    return document ? document->name() : std::string{};
+}
 
 void UIController::renameProject() {}
 
 void UIController::deleteProject() {}
 
 void UIController::closeApplication() {}
+
+void UIController::syncActiveDocument() {
+    Document* document = docManager_.getActiveDocument();
+    if (document == nullptr) {
+        return;
+    }
+
+    overlay_.clear();
+    const Document& activeDocument = *document;
+    builder_.setScene(activeDocument.scene());
+    picker_.setScene(activeDocument.scene());
+    host_.requestRedraw();
+}
 
 
 
--- src/app/UIController.h
+++ src/app/UIController.h
@@ -4,13 +4,21 @@
 #include <string>
 
 #include "DocumentManager.h"
+#include "Cpu2dPicker.h"
 #include "InteractionTools/EditorSession.h"
 #include "InteractionTools/ToolId.h"
 #include "IViewportHost.h"
+#include "OverlayModel.h"
+#include "RenderDataBuilder.h"
 
 class UIController {
 public:
-    UIController(EditorSession& editorSession, DocumentManager& manager, IViewportHost& host);
+    UIController(EditorSession& editorSession,
+                 DocumentManager& manager,
+                 IViewportHost& host,
+                 RenderDataBuilder& builder,
+                 Cpu2dPicker& picker,
+                 OverlayModel& overlay);
     ~UIController() = default;
 
     void selectTool(ToolId tool);
@@ -25,10 +33,14 @@ public:
     void openProjectInCurrentWindow();
     void createProjectInCurrentWindow();
 
-    void openFile();
+    std::string openFile(const std::string& filePath);
+    bool saveActiveDocument();
+    std::string saveActiveDocumentAs(const std::string& filePath);
+    bool setActiveDocument(const std::string& documentName);
+    std::string activeDocumentName();
     void renameTab();
     void removeTab();
-    void createFile();
+    std::string createFile(const std::string& documentName);
 
     void renameProject();
     void deleteProject();
@@ -39,6 +51,11 @@ private:
     EditorSession& editorSession_;
     DocumentManager& docManager_;
     IViewportHost& host_;
+    RenderDataBuilder& builder_;
+    Cpu2dPicker& picker_;
+    OverlayModel& overlay_;
+
+    void syncActiveDocument();
 };
 
 #endif // ! OURPAINT_APPLICATION_UI_CONTROLLER_H_
