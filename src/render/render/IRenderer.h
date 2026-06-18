0f8b87451d1014c3249f5a1fc6da45ad2c58919a
Ibragim Oskanov <oskanov-ibragim.06@mail.ru>
2026-06-18 17:15:52 +0300

    review render architecture: review RenderScene

--- src/render/render/IRenderer.h
+++ src/render/render/IRenderer.h
@@ -1,7 +1,7 @@
 #ifndef OURPAINT_RENDERING_IRENDERER_H_
 #define OURPAINT_RENDERING_IRENDERER_H_
 
-namespace renderer {class RenderData;}
+namespace render { class RenderScene; }
 class Camera2D;
 
 class IRenderer {
@@ -9,7 +9,7 @@ public:
     virtual bool initialize() = 0;
     virtual void shutdown() = 0;
     virtual void resize(int w, int h) = 0;
-    virtual void render(const renderer::RenderData& scene, const Camera2D& camera) = 0;
+    virtual void render(const render::RenderScene& scene, const Camera2D& camera) = 0;
     virtual ~IRenderer() = default;
 };
 
--- src/render/render/text/Types.h
+++ src/render/render/text/Types.h
@@ -6,7 +6,7 @@
 #include <string>
 #include <vector>
 
-namespace rendering::text {
+namespace render::text {
 // ------------------------------------------------------------
 // Basic types
 // ------------------------------------------------------------
